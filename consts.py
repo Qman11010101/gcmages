3972f16f165c4ffca42e186e2f188cfd9e6b13a5
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2025-05-18 16:33:16 +0900

    初期設定ファイルと依存関係を追加

---
+++ consts.py
@@ -0,0 +1,24 @@
+import platform
+
+JACKET_PATH = "/var/www/html/jacket/" if platform.system() != "Windows" else "./jacket"
+
+GAMES = {
+    "CHUNITHM": {
+        "title_small": "chunithm",
+        "image_base": "https://new.chunithm-net.com/chuni-mobile/html/mobile/img/",
+        "music_data": "https://chunithm.sega.jp/storage/json/music.json",
+        "image_key": "image",
+    },
+    "MAIMAI": {
+        "title_small": "maimai",
+        "image_base": "https://maimaidx.jp/maimai-mobile/img/Music/",
+        "music_data": "https://maimai.sega.jp/data/maimai_songs.json",
+        "image_key": "image_url",
+    },
+    "ONGEKI": {
+        "title_small": "ongeki",
+        "image_base": "https://ongeki-net.com/ongeki-mobile/img/music/",
+        "music_data": "https://ongeki.sega.jp/assets/json/music/music.json",
+        "image_key": "image_url",
+    },
+}
